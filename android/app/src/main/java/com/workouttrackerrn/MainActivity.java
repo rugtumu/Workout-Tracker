4d6daf519d38d5140526b32e1f6b3f6ad884ced1
Umut Gür <umutgur7@gmail.com>
2025-07-14 22:11:16 +0300

    React Native related files deleted

---
+++ android/app/src/main/java/com/workouttrackerrn/MainActivity.java
@@ -0,0 +1,6 @@
+package com.workouttrackerrn;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+} 
---
+++ android/app/src/main/java/com/workouttrackerrn/MainApplication.java
@@ -0,0 +1,6 @@
+package com.workouttrackerrn;
+
+import io.flutter.app.FlutterApplication;
+
+public class MainApplication extends FlutterApplication {
+} 
